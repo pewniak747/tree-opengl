a030bdc8778649192ce5374c9648bfcc8a49839d
Tomasz Pewiński <pewniak747@gmail.com>
2013-09-01 13:48:07 +0200

    modifying spherical coordinates

--- src/spherical_coordinates.cpp
+++ src/spherical_coordinates.cpp
@@ -5,3 +5,25 @@ SphericalCoordinates::SphericalCoordinates(float radialDistance, float polarAngl
   this->polarAngle = polarAngle;
   this->azimuthAngle = azimuthAngle;
 }
+
+void SphericalCoordinates::changeDistance(float delta) {
+  this->radialDistance += delta;
+  if(this->radialDistance < 0)
+    this->radialDistance = 0;
+}
+
+void SphericalCoordinates::changePolar(float delta) {
+  this->polarAngle += delta;
+  if(this->polarAngle < 0.0f)
+    this->polarAngle += 360.0f;
+  if(this->polarAngle > 360.0f)
+    this->polarAngle -= 360.f;
+}
+
+void SphericalCoordinates::changeAzimuth(float delta) {
+  this->azimuthAngle += delta;
+  if(this->azimuthAngle < 0.0f)
+    this->azimuthAngle = 0.0f;
+  if(this->azimuthAngle > 90.0f)
+    this->azimuthAngle = 90.0f;
+}
--- src/spherical_coordinates.h
+++ src/spherical_coordinates.h
@@ -4,6 +4,9 @@
 class SphericalCoordinates {
   public:
     SphericalCoordinates(float radialDistance, float polarAngle, float azimuthAngle);
+    void changeDistance(float delta);
+    void changePolar(float delta);
+    void changeAzimuth(float delta);
   private:
     float radialDistance;
     float polarAngle;
